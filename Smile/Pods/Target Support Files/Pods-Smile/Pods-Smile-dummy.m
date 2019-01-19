9a556fa0b2563ff2ae084df9c1d5e8091ce0f40a
李金埕 <851949383@qq.com>
2019-01-18 21:37:53 -0800

    init

---
+++ Smile/Pods/Target Support Files/Pods-Smile/Pods-Smile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Smile : NSObject
+@end
+@implementation PodsDummy_Pods_Smile
+@end
---
+++ Smile/Smile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smile
+//
+//  Created by 李金 on 1/18/19.
+//  Copyright © 2019 Kingandyoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
